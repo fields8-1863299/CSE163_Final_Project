709f9d413a8823d138a852db73c45fcb1b356ff3
Sam Fields <fields8@uw.edu>
2020-06-05 14:55:44 -0700

    sam finished flake8

--- plot_poverty_vs_residence.py
+++ plot_poverty_vs_residence.py
@@ -1,32 +1,34 @@
 from process_files import getDF_A as getDF
-import pandas as pd
 import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 
 
 def main():
     df = getDF()
-    df = df[(df['Category'] == 'Inside metropolitan statistical areas') | (df['Category'] == 'Inside principal cities') |
-            (df['Category'] == 'Inside principal cities') | (df['Category'] == 'Outside principal cities') |
+    df = df[(df['Category'] == 'Inside metropolitan statistical areas') |
+            (df['Category'] == 'Inside principal cities') |
+            (df['Category'] == 'Inside principal cities') |
+            (df['Category'] == 'Outside principal cities') |
             (df['Category'] == 'Outside metropolitan statistical areas')]
 
     categories_pop = ['2017 Total Population', '2018 Total Population']
-    categories_tot_pov = ['2017 Total Below Poverty', '2018 Total Below Poverty']
-    categories_per_pov = ['2017 Percentage Below Poverty', '2018 Percentage Below Poverty']
+    categories_tot_pov = ['2017 Total Below Poverty',
+                          '2018 Total Below Poverty']
+    categories_per_pov = ['2017 Percentage Below Poverty',
+                          '2018 Percentage Below Poverty']
     categories_inc = ['2017 Median Income', '2018 Median Income']
 
     fig_pop = go.Figure(data=[
         go.Bar(name='Inside metropolitan statistical areas', x=categories_pop,
-          y=df[df['Category'] == 'Inside metropolitan statistical areas']\
+               y=df[df['Category'] == 'Inside metropolitan statistical areas']
                .loc[18, categories_pop].to_numpy()),
         go.Bar(name='Inside principal cities', x=categories_pop,
-            y=df[df['Category'] == 'Inside principal cities'] \
+               y=df[df['Category'] == 'Inside principal cities']
                .loc[19, categories_pop].to_numpy()),
         go.Bar(name='Outside principal cities', x=categories_pop,
-            y=df[df['Category'] == 'Outside principal cities'] \
+               y=df[df['Category'] == 'Outside principal cities']
                .loc[20, categories_pop].to_numpy()),
         go.Bar(name='Outside metropolitan statistical areas', x=categories_pop,
-            y=df[df['Category'] == 'Outside metropolitan statistical areas'] \
+               y=df[df['Category'] == 'Outside metropolitan statistical areas']
                .loc[21, categories_pop].to_numpy())])
     fig_pop.update_layout(barmode='group',
                           title="2017 & 2018 Total Population by Residence",
@@ -34,58 +36,70 @@ def main():
     fig_pop.write_image("plots/R2017 & 2018 Total Population by Residence.png")
 
     fig_tot_pov = go.Figure(data=[
-        go.Bar(name='Inside metropolitan statistical areas', x=categories_tot_pov,
-               y=df[df['Category'] == 'Inside metropolitan statistical areas'] \
+        go.Bar(name='Inside metropolitan statistical areas',
+               x=categories_tot_pov,
+               y=df[df['Category'] == 'Inside metropolitan statistical areas']
                .loc[18, categories_tot_pov].to_numpy()),
         go.Bar(name='Inside principal cities', x=categories_tot_pov,
-               y=df[df['Category'] == 'Inside principal cities'] \
+               y=df[df['Category'] == 'Inside principal cities']
                .loc[19, categories_tot_pov].to_numpy()),
         go.Bar(name='Outside principal cities', x=categories_tot_pov,
-               y=df[df['Category'] == 'Outside principal cities'] \
+               y=df[df['Category'] == 'Outside principal cities']
                .loc[20, categories_tot_pov].to_numpy()),
-        go.Bar(name='Outside metropolitan statistical areas', x=categories_tot_pov,
-               y=df[df['Category'] == 'Outside metropolitan statistical areas'] \
+        go.Bar(name='Outside metropolitan statistical areas',
+               x=categories_tot_pov,
+               y=df[df['Category'] == 'Outside metropolitan statistical areas']
                .loc[21, categories_tot_pov].to_numpy())])
     fig_tot_pov.update_layout(barmode='group',
-                          title="2017 & 2018 Total Population Below Poverty Line by Residence",
-                          yaxis_title="Population Below Poverty Line")
-    fig_tot_pov.write_image("plots/2017 & 2018 Total Population Below Poverty Line by Residence.png")
+                              title="2017 & 2018 Total Population \
+                              Below Poverty Line by Residence",
+                              yaxis_title="Population Below Poverty Line")
+    fig_tot_pov.write_image("plots/2017 & 2018 Total Population Below \
+    Poverty Line by Residence.png")
 
     fig_per_pov = go.Figure(data=[
-        go.Bar(name='Inside metropolitan statistical areas', x=categories_per_pov,
-               y=df[df['Category'] == 'Inside metropolitan statistical areas'] \
+        go.Bar(name='Inside metropolitan statistical areas',
+               x=categories_per_pov,
+               y=df[df['Category'] == 'Inside metropolitan statistical areas']
                .loc[18, categories_per_pov].to_numpy()),
         go.Bar(name='Inside principal cities', x=categories_per_pov,
-               y=df[df['Category'] == 'Inside principal cities'] \
+               y=df[df['Category'] == 'Inside principal cities']
                .loc[19, categories_per_pov].to_numpy()),
         go.Bar(name='Outside principal cities', x=categories_per_pov,
-               y=df[df['Category'] == 'Outside principal cities'] \
+               y=df[df['Category'] == 'Outside principal cities']
                .loc[20, categories_per_pov].to_numpy()),
-        go.Bar(name='Outside metropolitan statistical areas', x=categories_per_pov,
-               y=df[df['Category'] == 'Outside metropolitan statistical areas'] \
+        go.Bar(name='Outside metropolitan statistical areas',
+               x=categories_per_pov,
+               y=df[df['Category'] ==
+                    'Outside metropolitan statistical areas']
                .loc[21, categories_per_pov].to_numpy())])
     fig_per_pov.update_layout(barmode='group',
-                          title="2017 & 2018 Percentage of Population Below Poverty Line by Residence",
-                          yaxis_title="Percentage")
-    fig_per_pov.write_image("plots/2017 & 2018 Percentage of Population Below Poverty Line by Residence.png")
+                              title="2017 & 2018 Percentage of Population \
+                              Below Poverty Line by Residence",
+                              yaxis_title="Percentage")
+    fig_per_pov.write_image(
+        "plots/2017 & 2018 Percentage of Population Below \
+        Poverty Line by Residence.png")
 
     fig_inc = go.Figure(data=[
         go.Bar(name='Inside metropolitan statistical areas', x=categories_inc,
-               y=df[df['Category'] == 'Inside metropolitan statistical areas'] \
+               y=df[df['Category'] == 'Inside metropolitan statistical areas']
                .loc[18, categories_inc].to_numpy()),
         go.Bar(name='Inside principal cities', x=categories_inc,
-               y=df[df['Category'] == 'Inside principal cities'] \
+               y=df[df['Category'] == 'Inside principal cities']
                .loc[19, categories_inc].to_numpy()),
         go.Bar(name='Outside principal cities', x=categories_inc,
-               y=df[df['Category'] == 'Outside principal cities'] \
+               y=df[df['Category'] == 'Outside principal cities']
                .loc[20, categories_inc].to_numpy()),
         go.Bar(name='Outside metropolitan statistical areas', x=categories_inc,
-               y=df[df['Category'] == 'Outside metropolitan statistical areas'] \
+               y=df[df['Category'] ==
+                    'Outside metropolitan statistical areas']
                .loc[21, categories_inc].to_numpy())])
     fig_inc.update_layout(barmode='group',
                           title="2017 & 2018 Median Income by Residence",
                           yaxis_title="Income")
     fig_inc.write_image("plots/2017 & 2018 Median Income by Residence.png")
+
 
 if __name__ == '__main__':
     main()
--- process_files.py
+++ process_files.py
@@ -1,48 +1,57 @@
 import pandas as pd
 import re
 
+
 def getDF_A():
     d1 = pd.read_csv("cse 163 datasets/A/ds1.csv")
     d2 = pd.read_csv("cse 163 datasets/A/ds2.csv")
     d5 = pd.read_csv("cse 163 datasets/A/ds5.csv")
 
     d1 = d1.loc[12:62, :]
-    d1.columns = ['Category', '2017 Total Population', '2017 Total Below Poverty', 'drop',
+    d1.columns = ['Category', '2017 Total Population',
+                  '2017 Total Below Poverty', 'drop',
                   '2017 Percentage Below Poverty',
-                  'drop', '2018 Total Population', '2018 Total Below Poverty', 'drop', '2018 Percentage Below Poverty',
+                  'drop', '2018 Total Population', '2018 Total Below Poverty',
+                  'drop', '2018 Percentage Below Poverty',
                   'drop', 'drop', 'drop']
     d1 = d1.loc[:, d1.columns != 'drop']
     d1 = d1.dropna()
-    # d1["Category"].replace({"Under age 18": "Aged Under 65",
-    #                         "Aged 18 to 64": "Aged Under 65",
-    #                         "Aged 65 and older": "Aged Over 65"
-    #                         }, inplace=True)
 
     d2 = d2.loc[9:12, :]
-    d2.columns = ['Category', '2017 Total Population', '2017 Total Below Poverty', 'drop',
+    d2.columns = ['Category', '2017 Total Population',
+                  '2017 Total Below Poverty', 'drop',
                   '2017 Percentage Below Poverty',
-                  'drop', '2018 Total Population', '2018 Total Below Poverty', 'drop', '2018 Percentage Below Poverty',
+                  'drop', '2018 Total Population', '2018 Total Below Poverty',
+                  'drop', '2018 Percentage Below Poverty',
                   'drop', 'drop', 'drop', 'drop']
     d2 = d2.loc[:, d2.columns != 'drop']
     d2 = d2.dropna()
-    d2["Category"].replace({"Primary Families4…………………………………": "Primary Families"
-                            }, inplace=True)
+    d2["Category"].replace({"Primary Families4…………………………………":
+                            "Primary Families"}, inplace=True)
 
     d5 = d5.loc[6:43, :]
-    d5.columns = ['Category', '2017 Number People', '2017 Median Income', 'drop', '2018 Number People',
-                  '2018 Median Income', 'drop', 'drop', 'drop', 'drop', 'drop', 'drop', 'drop', 'drop', 'drop',
-                  'drop', 'drop', 'drop', 'drop', 'drop', 'drop', 'drop', 'drop', 'drop', 'drop', 'drop', 'drop',
-                  'drop', 'drop', 'drop', 'drop', 'drop', 'drop', 'drop', 'drop', 'drop', 'drop', 'drop']
+    d5.columns = ['Category', '2017 Number People', '2017 Median Income',
+                  'drop', '2018 Number People',
+                  '2018 Median Income', 'drop', 'drop', 'drop',
+                  'drop', 'drop', 'drop', 'drop', 'drop', 'drop',
+                  'drop', 'drop', 'drop', 'drop', 'drop', 'drop',
+                  'drop', 'drop', 'drop', 'drop', 'drop', 'drop',
+                  'drop', 'drop', 'drop', 'drop', 'drop', 'drop',
+                  'drop', 'drop', 'drop', 'drop', 'drop']
     d5 = d5.loc[:, d5.columns != 'drop']
     d5 = d5.dropna()
     d5['Category'].replace({".  Married-couple": "Married-couple",
-                            ".  Female householder, no spouse present": "Female householder, no spouse present",
-                            ".  Male householder, no spouse present": "Male householder, no spouse present",
+                            ".  Female householder, no spouse present":
+                                "Female householder, no spouse present",
+                            ".  Male householder, no spouse present":
+                                "Male householder, no spouse present",
                             "   White, not Hispanic": "White, not Hispanic",
                             ".. Naturalized citizen": "Naturalized citizen",
                             ".. Not a citizen": "Not a citizen",
-                            ".. Inside principal cities": "Inside principal cities",
-                            ".. Outside principal cities": "Outside principal cities",
+                            ".. Inside principal cities":
+                                "Inside principal cities",
+                            ".. Outside principal cities":
+                                "Outside principal cities",
                             "Under 65 years": "Aged Under 65",
                             "65 years and older": "Aged Over 65"
                             }, inplace=True)
@@ -52,13 +61,16 @@ def getDF_A():
 
     df = df.merge(d5, left_on='Category', right_on='Category', how='outer')
 
-    columns = ['2017 Total Population', '2017 Total Below Poverty', '2018 Total Population', '2018 Total Below Poverty',
-               '2017 Number People', '2017 Median Income', '2018 Number People', '2018 Median Income']
+    columns = ['2017 Total Population', '2017 Total Below Poverty',
+               '2018 Total Population', '2018 Total Below Poverty',
+               '2017 Number People', '2017 Median Income',
+               '2018 Number People', '2018 Median Income']
 
     for col in columns:
         df[col] = df[col].apply(lambda x: re.sub('[^A-Za-z0-9]+', '', str(x)))
 
     return df
+
 
 def getDF_B():
     associates = getDegree("Associates", 26)
@@ -70,15 +82,21 @@ def getDF_B():
     df = pd.concat(frames)
     return df
 
+
 def getDegree(degree, ind):
     df = pd.read_csv("cse 163 datasets/B/" + degree + "_csv.csv")
     df = df.loc[[ind], :]
-    df.columns = ['Degree', 'Num Total', 'Num White', 'Num Black', 'Num Hispanic', 'Num Asian/Pacific Islander',
-                  'Num American Indian/ Alaskan native', 'Num Two or More', 'Num Non Resident', 'Perc Total',
-                  'Perc White', 'Perc Black', 'Perc Hispanic', 'Perc Asian/Pacific Islander',
+    df.columns = ['Degree', 'Num Total', 'Num White',
+                  'Num Black', 'Num Hispanic',
+                  'Num Asian/Pacific Islander',
+                  'Num American Indian/ Alaskan native', 'Num Two or More',
+                  'Num Non Resident', 'Perc Total',
+                  'Perc White', 'Perc Black', 'Perc Hispanic',
+                  'Perc Asian/Pacific Islander',
                   'Perc American Indian/ Alaskan native', 'Perc Two or More']
     df['Degree'] = degree
     return df
+
 
 def getDF_C():
     df_C = pd.read_csv("cse 163 datasets/C/state_csv.csv")
@@ -88,10 +106,13 @@ def getDF_C():
     df_C.columns = ['State', '2017-2018 Percentage Poverty']
     return df_C
 
+
 def getDF_D():
     df = pd.read_csv("cse 163 datasets/D/tableA1_csv.csv")
-    df = df.loc[16:20, ['Table with row headers in column A and column headers in row 4 through 6.', 'Unnamed: 1',
+    df = df.loc[16:20, ['Table with row headers in column A and column \
+                        headers in row 4 through 6.', 'Unnamed: 1',
                         'Unnamed: 2', 'Unnamed: 4', 'Unnamed: 5']]
-    df.columns = ['Race', '2017 Number (thousands)', '2017 Median Income', '2018 Number (thousands)',
+    df.columns = ['Race', '2017 Number (thousands)', '2017 Median Income',
+                  '2018 Number (thousands)',
                   '2018 Median Income']
     return df
